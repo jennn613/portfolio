45d12373150d8fe0ab5ac01a5053dd5a891df660
Jenny <69027218+jennn613@users.noreply.github.com>
2020-10-18 12:36:38 +0900

    home opacity

--- js/main.js
+++ js/main.js
@@ -13,11 +13,12 @@ document.addEventListener('scroll', () => {
   }
   });
 
-//Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click', (event) => {
-
-  const target = event.target;
+  
+  //Handle scrolling when tapping on the navbar menu
+  const navbarMenu = document.querySelector('.navbar__menu');
+  navbarMenu.addEventListener('click', (event) => {
+    
+    const target = event.target;
   const link= target.dataset.link;
   if(link == null){
     return;
@@ -31,5 +32,15 @@ contactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
 })
 
-  function scrollIntoView(selector){const scrollTo = document.querySelector(selector);
+function scrollIntoView(selector){const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});}
+
+    //make home fade in
+  
+  const home = document.querySelector('.home__container');
+  const homeHeight = home.getBoundingClientRect().height;
+  document.addEventListener('scroll', () => {
+
+      home.style.opacity = 1 - window.scrollY / homeHeight;
+    
+  });
